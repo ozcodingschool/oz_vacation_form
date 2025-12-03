63d5518024700a159a0adc871932f97212167f5e
강성구 <sungku2757@gmail.com>
2025-12-03 19:01:23 +0900

    hotfix: env setting fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // 필요에 따라 추가 설정
-  envPrefix: "REACT_APP_", // 환경 변수 prefix 변경 등
   build: {
     outDir: "build", // 빌드 출력 디렉토리 설정
   },
